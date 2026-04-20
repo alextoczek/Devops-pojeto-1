c7077272b7210c78cbd2be5cc8fb788194274b56
alextoczek <alexsilvatoczek@gmail.com>
2026-04-19 22:52:54 -0300

    Adiciona 5 testes unitários

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 import json
 import os
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return jsonify({"mensagem": "API funcionando"})
 
-# carregar dados direto (sem funções separadas)
 def carregar_estudantes():
     if not os.path.exists('estudantes.json'):
         return {}
     with open('estudantes.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
-
-# página inicial
 @app.route('/')
-def inicio():
-    return render_template('index.html')
+def home():
+    return jsonify({"mensagem": "API funcionando"})
 
-
-# rota simples para listar estudantes
 @app.route('/estudantes')
 def listar_estudantes():
     dados = carregar_estudantes()
     return jsonify(dados)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
